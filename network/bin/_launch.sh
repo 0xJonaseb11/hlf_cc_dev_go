f5e3bcdfd8e1a739d955e32edb1bfa9c10e72634
Jaz-3-0 <sebejaz99@gmail.com>
2024-03-07 08:37:59 +0200

    update init

--- network/bin/_launch.sh
+++ network/bin/_launch.sh
@@ -1,4 +1,4 @@
 #PEER_MODE=net
-#Command=dev-init.sh -e 
-#Generated: Sat Mar  2 15:10:00 UTC 2024 
-docker-compose  -f ./compose/docker-compose.base.yaml      -f ./compose/docker-compose.explorer.yaml    up -d --remove-orphans
+#Command=dev-init.sh  
+#Generated: Thu Mar  7 05:05:45 UTC 2024 
+docker-compose  -f ./compose/docker-compose.base.yaml        up -d --remove-orphans
