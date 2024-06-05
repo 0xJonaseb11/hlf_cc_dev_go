8e8838d434f1e7e2b6ffe48d34e06173860e8a0f
Jaz-3-0 <sebejaz99@gmail.com>
2024-06-05 08:50:38 +0200

    update fixes

--- network/bin/cc.env.sh
+++ network/bin/cc.env.sh
@@ -1,13 +1,13 @@
-# Generated: Wed Jun  5 04:32:53 UTC 2024
+# Generated: Wed Jun  5 06:06:15 UTC 2024
 export CC_LANGUAGE=golang
-export CC_PATH=token/v3
-export CC_NAME=token
+export CC_PATH=chaincode_example02
+export CC_NAME=gocc
 export CC_VERSION=1.0
 export CC_CHANNEL_ID=airlinechannel
 export CC_CONSTRUCTOR='{"Args":["init","a","100","b","300"]}'
 export CC_QUERY_ARGS='{"Args":["query","b"]}'
 export CC_INVOKE_ARGS='{"Args":["invoke","a","b","5"]}'
-export CORE_CHAINCODE_ID_NAME='token'
+export CORE_CHAINCODE_ID_NAME='gocc'
 export CORE_CHAINCODE_LOGGING_LEVEL=''
 export CORE_CHAINCODE_LOGGING_SHIM=''
 export CC_PRIVATE_DATA_JSON=''
